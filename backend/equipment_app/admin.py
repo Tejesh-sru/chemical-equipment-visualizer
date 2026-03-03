3fa73ed5bfb0488da5d10b5e3cb2589eca881050
Tejesh Jakkula <jakkulatejesh2005@gmail.com>
2026-03-03 20:02:36 +0530

    Update admin.py

--- backend/equipment_app/admin.py
+++ backend/equipment_app/admin.py
@@ -5,7 +5,7 @@ from .models import UploadedDataset, Equipment
 @admin.register(UploadedDataset)
 class UploadedDatasetAdmin(admin.ModelAdmin):
     list_display = ['filename', 'upload_date', 'equipment_count', 'avg_flowrate', 'avg_pressure']
-    list_filter = ['upload_date']
+    list_filter = ['upload_date'] 
     search_fields = ['filename']
     readonly_fields = ['avg_flowrate', 'avg_pressure', 'avg_temperature', 'type_distribution']
 
